f75891ec0c7e02dbcc65e42340f0709e559b9247
SanfordLuo <864938527@qq.com>
2018-06-26 17:02:09 +0800

    完成注册

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -1,7 +1,7 @@
-var vm = new Vue({
-	el: '#app',
-	data: {
-	    host,
+var vm = new Vue({   // vue实例
+	el: '#app',      // 绑定标签到vue
+	data: {　　　　　　// 定义的变量
+		host,
 		error_name: false,
 		error_password: false,
 		error_check_password: false,
@@ -13,24 +13,26 @@ var vm = new Vue({
 		username: '',
 		password: '',
 		password2: '',
-		mobile: '', 
+		mobile: '',
 		image_code: '',
 		sms_code: '',
 		allow: false,
 
-		image_code_id: '', // uuid
-		image_code_url: '', // 索取图片验证码的url
+		image_code_id: '',  //　定义uuid
+		image_code_url: '',  // 索取图片验证码的url
 		sending_flag: false,  // 发送短信标志
-		sms_code_tip: '获取短信验证码', //发送短信标签的提示文字
+		sms_code_tip: '获取短信验证码',  //发送短信标签的提示文字
 		error_image_code_message: '',  //图片验证码错误信息提示
-
+		error_name_message: '', // 用户名错误提示
+		error_phone_message: '', // 手机号码错误提示
+		error_sms_code_message: '' // 短信验证码错误提示
 	},
-	mounted: function () {
-		// 调用图片验证码的方法
+	mounted: function () {  // 文档加载完后执行的，可以调用方法
+		// 调用获取图片验证码的方法
 		this.generate_image_code();
     },
-	methods: {
-		// 生成UUID的方法
+	methods: {      //　要被调用的方法
+		// 生成uuid
 		generate_uuid: function(){
 			var d = new Date().getTime();
 			if(window.performance && typeof window.performance.now === "function"){
@@ -42,54 +44,96 @@ var vm = new Vue({
 				return (c =='x' ? r : (r&0x3|0x8)).toString(16);
 			});
 			return uuid;
-    	},
-		// 生成图片验证码的方法
-		generate_image_code: function () {
-			// 生成UUID
-			this.image_code_id = this.generate_uuid();
-			// 拼接访问图片验证码接口的url
-			this.image_code_url = this.host + '/image_codes/' + this.image_code_id + '/';
-			// 并给img标签的src属性赋值url,在register.html中实现
-			// <img :src="image_code_url">
-        },
-
-		check_username: function (){
-			var len = this.username.length;
-			if(len<5||len>20) {
-				this.error_name = true;
-			} else {
-				this.error_name = false;
-			}
 		},
+
+		// 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
+		generate_image_code: function(){
+			//生成uuid
+			this.image_code_id = this.generate_uuid();
+
+			//拼接访问图片验证码接口的url
+			// this.image_code_url = 'http://127.0.0.1:8000' + "/image_codes/" + this.image_code_id + "/";
+			this.image_code_url = this.host + "/image_codes/" + this.image_code_id + "/";
+
+			//并给img标签的src属性赋值url，在register.html实现
+			// <img :src="image_code_url"
+		},
+
+		// 检查用户名
+		check_username: function (){
+            var len = this.username.length;
+            if(len<5||len>20) {
+                this.error_name_message = '请输入5-20个字符的用户名';
+                this.error_name = true;
+            } else {
+                this.error_name = false;
+            }
+            // 检查重名
+            if (this.error_name == false) {
+                axios.get(this.host + '/usernames/' + this.username + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
+        },
 		check_pwd: function (){
 			var len = this.password.length;
 			if(len<8||len>20){
 				this.error_password = true;
 			} else {
 				this.error_password = false;
-			}		
+			}
 		},
 		check_cpwd: function (){
 			if(this.password!=this.password2) {
 				this.error_check_password = true;
 			} else {
 				this.error_check_password = false;
-			}		
-		},
-		check_phone: function (){
-			var re = /^1[345789]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_phone = false;
-			} else {
-				this.error_phone = true;
 			}
 		},
+		// 检查手机号
+		check_phone: function (){
+            var re = /^1[345789]\d{9}$/;
+            if(re.test(this.mobile)) {
+                this.error_phone = false;
+            } else {
+                this.error_phone_message = '您输入的手机号格式不正确';
+                this.error_phone = true;
+            }
+            if (this.error_phone == false) {
+                axios.get(this.host + '/mobiles/'+ this.mobile + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_phone_message = '手机号已存在';
+                            this.error_phone = true;
+                        } else {
+                            this.error_phone = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
+        },
+
 		check_image_code: function (){
 			if(!this.image_code) {
 				this.error_image_code = true;
 			} else {
 				this.error_image_code = false;
-			}	
+			}
 		},
 		check_sms_code: function(){
 			if(!this.sms_code){
@@ -105,7 +149,6 @@ var vm = new Vue({
 				this.error_allow = false;
 			}
 		},
-
 		// 发送手机短信验证码
         send_sms_code: function(){
             if (this.sending_flag == true) {
@@ -124,7 +167,7 @@ var vm = new Vue({
 
             // 向后端接口发送请求，让后端发送短信验证码
             axios.get(this.host + '/sms_codes/' + this.mobile + '/?text=' + this.image_code+'&image_code_id='+ this.image_code_id, {
-                    responseType: 'json' // 要求后端给vue响应json
+                    responseType: 'json'
                 })
                 .then(response => {
                     // 表示后端发送短信成功
@@ -157,15 +200,44 @@ var vm = new Vue({
                     this.sending_flag = false;
                 })
         },
-
 		// 注册
-		on_submit: function(){
-			this.check_username();
-			this.check_pwd();
-			this.check_cpwd();
-			this.check_phone();
-			this.check_sms_code();
-			this.check_allow();
-		}
+        on_submit: function(){
+            this.check_username();
+            this.check_pwd();
+            this.check_cpwd();
+            this.check_phone();
+            this.check_sms_code();
+            this.check_allow();
+
+            if(this.error_name == false && this.error_password == false && this.error_check_password == false
+                && this.error_phone == false && this.error_sms_code == false && this.error_allow == false) {
+                axios.post(this.host + '/users/', {
+                        username: this.username,
+                        password: this.password,
+                        password2: this.password2,
+                        mobile: this.mobile,
+                        sms_code: this.sms_code,
+                        allow: this.allow.toString()
+                    }, {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        location.href = '/index.html';
+                    })
+                    .catch(error=> {
+                        if (error.response.status == 400) {
+                            if ('non_field_errors' in error.response.data) {
+                                this.error_sms_code_message = error.response.data.non_field_errors[0];
+                            } else {
+                                this.error_sms_code_message = '数据有误';
+                            }
+                            this.error_sms_code = true;
+                        } else {
+                            console.log(error.response.data);
+                        }
+                    })
+            }
+        }
+
 	}
 });
